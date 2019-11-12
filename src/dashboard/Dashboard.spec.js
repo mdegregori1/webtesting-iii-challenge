24062a5a7acbbac72efbbb685dffca2e14d07bab
Mauricio Degregori <degregori2@gmail.com>
2019-11-11 19:05:54 -0600

    set up render + snapshot testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
-// Test away
+import React from "react";
+import { render } from '@testing-library/react';
+
+import Dashboard from "./Dashboard";
+import Controls from "../controls/Controls";
+import Display from "../display/Display";
+
+// rendering / text file 
+test('Dashboard renders correctly ', ()=> {
+    expect(render(<Dashboard/>)).toMatchSnapshot();
+})
+
+// Dashboard contains Controls 
+
+
+test('Dashboard contains controls', () => {
+    render(<Controls/>);
+  });
+
+//Dashboard contains Displays
+
+test('Dashboard contains Display', () => {
+    render(<Display/>);
+});
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,43 @@
-// Test away!
+import React from "react";
+import { render } from '@testing-library/react';
+
+import Display from "./Display";
+
+test('Dashboard renders correctly ', ()=> {
+    expect(render(<Display/>)).toMatchSnapshot();
+})
+
+// What do we need to do ?
+
+// 1. 
+// Test if screen displays when the gate is opened and closed 
+// Clicked on buttons 
+// check if there is an open and close button 
+test('Gate defaults to unlocked and opened', () => {
+    const { getByText } = render(<Display />);
+    getByText(/unlocked/i);
+    getByText(/open/i)
+  });
+
+
+// 2.
+// Test if screen displays when gate is locked and unlocked
+// Clicked on buttons 
+// check if there is a locked and an unlocked button 
+
+// 3. 
+// Test if screen shows closed if closed = true, displays open if otherwise 
+
+// 4. 
+// Test if screen displays locked if locked=true , displays unlocked if otherwise
+
+// 5. 
+// When clicked on locked = red-led class
+// When clicked on closed = red-led class
+
+
+
+// 6. 
+// When clicked on unlocked = "green-led" class
+// When clicked on open = "green-led" class 
+
